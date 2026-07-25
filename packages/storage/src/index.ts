0e1a3614e118c3a747a3713148bad6a22785430f
THRISHAL12345 <domathrishal123@gmail.com>
2026-07-26 00:18:59 +0530

    fix(storage): resolve __dirname instead of import.meta.url for CommonJS support

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -4,7 +4,7 @@ import * as os from "os";
 
 // For local v1 development, we use a hidden folder in the monorepo root.
 // In production, this would be backed by S3.
-const STORAGE_DIR = path.resolve(process.cwd(), "../../.vigil-storage");
+const STORAGE_DIR = path.resolve(__dirname, "../../../.vigil-storage");
 
 async function ensureStorageDir() {
   try {
